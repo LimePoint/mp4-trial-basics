8a2fd10674787d15f997794bc7155cd7abfbc572
Romil Bhagat <romil@limepoint.com>
2025-10-08 16:45:56 +1000

    draft

--- actions.rb
+++ actions.rb
@@ -1,8 +1,8 @@
 # Simple exec
 #
  action :run_shell_commands do
-  sh 'export MY_VAR=foo; echo value of my_var in the same sh: $MY_VAR'
-  sh 'echo value of my_var in the subsequent sh: $MY_VAR'
+  sh 'export MY_VAR=foo; echo value of my_var in the same sh: ${MY_VAR}'
+  sh 'echo value of my_var in the subsequent sh: ${MY_VAR}' # variables from previous cannot be used in subsequent calls, each is run in its own shell
   sh 'echo default shell: ${SHELL}'
   sh "echo this will use bash echo command to print this line"
   sh "echo this will use bash echo command to print this second line"
