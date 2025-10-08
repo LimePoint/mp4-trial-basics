c3adbff0f39472504544e987e1cbbf6441481997
Romil Bhagat <romil@limepoint.com>
2025-10-08 16:33:47 +1000

    draft

--- actions.rb
+++ actions.rb
@@ -1,7 +1,10 @@
 # Simple exec
 #
- action :hello do
+ action :run_shell_commands do
+  sh 'echo default shell: ${SHELL}'
   sh "echo this will use bash echo command to print this line"
   sh "echo this will use bash echo command to print this second line"
   sh "echo 'this will print list of users'; cat /etc/passwd"
 end
+
+
