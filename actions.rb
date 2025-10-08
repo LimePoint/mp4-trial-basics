58a1e4ace13e84d59a3097651eb8b292eecdd3db
Romil Bhagat <romil@limepoint.com>
2025-10-09 09:10:32 +1000

    draft

--- actions.rb
+++ actions.rb
@@ -7,6 +7,7 @@ action :run_shell_commands do
   sh "echo 'this will print list of users'; cat /etc/passwd"
   sh 'MY_VAR=test; echo value of my_var in the same sh: ${MY_VAR}'
   sh 'echo value of my_var in the subsequent sh: ${MY_VAR}' # variables from previous cannot be used in subsequent calls, each is run in its own shell
+  sh 'echo value of variable my_value from outside of action block: #{my_value}'
 end
 
 # simple scripts
@@ -20,3 +21,8 @@ OpsChain.logger.info("----------- logger info --------- ")
 my_value = 'set_from_outside_action'
 
 OpsChain.logger.info ("Value of my_value = #{my_value}")
+
+# we can write any ruby code here, it will just execute coz mint will just run the full file and ignore anything under the actions block unless the 
+# action is called
+#
+
