bbf768913735b2e716d4b3a500ed632684f7570b
Romil Bhagat <romil@limepoint.com>
2025-10-08 16:50:57 +1000

    draft

--- actions.rb
+++ actions.rb
@@ -1,12 +1,16 @@
-# Simple exec
-#
- action :run_shell_commands do
-  sh 'export MY_VAR=foo; echo value of my_var in the same sh: ${MY_VAR}'
-  sh 'echo value of my_var in the subsequent sh: ${MY_VAR}' # variables from previous cannot be used in subsequent calls, each is run in its own shell
+
+# simple shell commands
+action :run_shell_commands do
   sh 'echo default shell: ${SHELL}'
   sh "echo this will use bash echo command to print this line"
   sh "echo this will use bash echo command to print this second line"
   sh "echo 'this will print list of users'; cat /etc/passwd"
+  sh 'MY_VAR=foo; echo value of my_var in the same sh: ${MY_VAR}'
+  sh 'echo value of my_var in the subsequent sh: ${MY_VAR}' # variables from previous cannot be used in subsequent calls, each is run in its own shell
 end
 
+# simple scripts
 
+action :run_script do
+  sh 'sh scripts/hello.sh'
+end
---
+++ scripts/hello.sh
@@ -0,0 +1,1 @@
+echo "this is a hello from within the $0 script."
