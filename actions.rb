83e1da570d8de3ba8e02286a34d828447963a11d
Romil Bhagat <romil@limepoint.com>
2025-10-08 16:44:09 +1000

    draft

--- actions.rb
+++ actions.rb
@@ -1,7 +1,7 @@
 # Simple exec
 #
  action :run_shell_commands do
-  sh 'MY_VAR=foo; echo value of my_var in the same sh: $MY_VAR'
+  sh 'export MY_VAR=foo; echo value of my_var in the same sh: $MY_VAR'
   sh 'echo value of my_var in the subsequent sh: $MY_VAR'
   sh 'echo default shell: ${SHELL}'
   sh "echo this will use bash echo command to print this line"
