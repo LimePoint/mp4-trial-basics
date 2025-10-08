e465639e387f7ffd682c30d4678c50adc97e82c1
Romil Bhagat <romil@limepoint.com>
2025-10-09 08:58:19 +1000

    draft

--- actions.rb
+++ actions.rb
@@ -5,12 +5,11 @@ action :run_shell_commands do
   sh "echo this will use bash echo command to print this line"
   sh "echo this will use bash echo command to print this second line"
   sh "echo 'this will print list of users'; cat /etc/passwd"
-  sh 'MY_VAR=foo; echo value of my_var in the same sh: ${MY_VAR}'
+  sh 'MY_VAR=test; echo value of my_var in the same sh: ${MY_VAR}'
   sh 'echo value of my_var in the subsequent sh: ${MY_VAR}' # variables from previous cannot be used in subsequent calls, each is run in its own shell
 end
 
 # simple scripts
-
 action :run_script do
   sh 'sh scripts/hello.sh'
 end
