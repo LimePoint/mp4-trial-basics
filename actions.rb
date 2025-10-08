06626811798f7b37e7d07383b72d96960e464852
Romil Bhagat <romil@limepoint.com>
2025-10-08 16:30:53 +1000

    draft

--- actions.rb
+++ actions.rb
@@ -3,4 +3,5 @@
  action :hello do
   sh "echo this will use bash echo command to print this line"
   sh "echo this will use bash echo command to print this second line"
+  sh "echo 'this will print list of users'; cat /etc/passwd"
 end
