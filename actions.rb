573598e883a7be8075afe99112062c92c39d65b0
Romil Bhagat <romil@limepoint.com>
2025-10-09 10:36:39 +1000

    draft

--- actions.rb
+++ actions.rb
@@ -79,3 +79,6 @@ action :parent_1, steps: [:child_1, :child_2] do
   OpsChain.logger.info "I am parent action 1 and will now call child_1 and child_2 one by one"
 end
 
+action :parent_2, steps: [:child_1, :child_2], run_as: parallel do
+  OpsChain.logger.info "I am parent action 2 and will now call child_1 and child_2 in parallel"
+end
