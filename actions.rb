dbd6e27021faeb0ab5de51548974a6f1c92fd9de
Romil Bhagat <romil@limepoint.com>
2025-10-09 15:27:43 +1000

    draft

--- actions.rb
+++ actions.rb
@@ -117,7 +117,7 @@ action :post_wait do
   OpsChain.logger.info "this will run after a wait step"
 end
 
-action :multiple_wait_steps, steps: [OpsChain.wait_step, :post_wait, OpsChain.wait_step, :post_wait] do
+action :multiple_wait_steps, steps: [OpsChain.wait_step, :post_wait, OpsChain.wait_step, :run_python] do
   OpsChain.logger.info "this is multiple wait steps"
 end
 
