20eec0211c9d2318507fec39d43dbdc5dafa7292
Romil Bhagat <romil@limepoint.com>
2025-10-13 08:00:11 +1100

    draft

--- actions.rb
+++ actions.rb
@@ -1,7 +1,6 @@
 action :run_multi_shell_commands do
   exec_command '/bin/true'
   exec_command 'whoami' # this will not run
-  exec_command '/bin/hostname' # this will not run
 end
 
 log.info "------------ shorter log"
