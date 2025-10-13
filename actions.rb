8677e15e9cfef33a64df03f82bbab174d42b61e6
Romil Bhagat <romil@limepoint.com>
2025-10-14 07:12:40 +1100

    draft

--- actions.rb
+++ actions.rb
@@ -1,3 +1,20 @@
+my_name = 'foo'
+
+action :print_variable_1 do
+  puts "--------- Value of variable my_name: #{my_name}"
+  puts "--------- Value of variable my_address: #{my_address}"
+end
+
+my_address = 'bar'
+
+action :print_variable_2 do
+  puts "--------- Value of variable my_name: #{my_name}"
+  puts "--------- Value of variable my_address: #{my_address}"
+end
+
+
+
+
 action :run_multi_shell_commands do
   exec_command '/tmp/script.sh' #, abort_on_failure: false
   puts "I will never get printed"
