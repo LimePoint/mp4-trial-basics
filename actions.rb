b468d6a95dfd6b576791b5d94e79be869ad49622
Romil Bhagat <romil@limepoint.com>
2025-10-14 07:24:07 +1100

    draft

--- actions.rb
+++ actions.rb
@@ -1,17 +1,30 @@
 my_name = 'foo'
+my_address = 'foo'
 
-action :print_variable_1 do
+action :print_variable do
   puts "--------- Value of variable my_name: #{my_name}"
   puts "--------- Value of variable my_address: #{my_address}"
 end
 
 my_address = 'bar'
 
-action :print_variable_2 do
-  puts "--------- Value of variable my_name: #{my_name}"
-  puts "--------- Value of variable my_address: #{my_address}"
-end
-
+#
+#
+#
+#my_name = 'foo'
+#
+#action :print_variable_1 do
+#  puts "--------- Value of variable my_name: #{my_name}"
+#  puts "--------- Value of variable my_address: #{my_address}"
+#end
+#
+#my_address = 'bar'
+#
+#action :print_variable_2 do
+#  puts "--------- Value of variable my_name: #{my_name}"
+#  puts "--------- Value of variable my_address: #{my_address}"
+#end
+#
 
 
 
