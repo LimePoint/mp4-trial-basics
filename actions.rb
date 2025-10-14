94a0af179339d1d51578ed2e6f4917126aab18bb
Romil Bhagat <romil@limepoint.com>
2025-10-14 14:03:42 +1100

    draft

--- actions.rb
+++ actions.rb
@@ -37,7 +37,7 @@ end
 
 action :use_logging do
   puts "Current log level - #{log.level}"
-  log.level = ::Logger::DEBUG
+  log.level = ::Logger::ERROR
   puts "new log level - #{log.level}"
   puts "I am being printed via puts"
   log.info "I am being printed via the logger as info"
