3caefb7821ba8f5403bb8c810c9328d11de8fccc
Romil Bhagat <romil@limepoint.com>
2025-10-17 09:59:11 +1100

    draft

--- actions.rb
+++ actions.rb
@@ -281,7 +281,8 @@ action :cleanup_temp_files do
 end
 
 action :kv_properties do
-  log.info "Total number of employees: #{OpsChain.properties.organization.employees.length}"
+  log.info "Total number of employees: #{OpsChain.properties}"
+  #log.info "Total number of employees: #{OpsChain.properties.organization.employees.length}"
   log.info "Value of env variable MY_VARIABLE: #{ENV['MY_VARIABLE']}"
   log.info "Contents of file at /tmp/myfile.txt - #{File.read('/tmp/myfile.txt')}"
 end
