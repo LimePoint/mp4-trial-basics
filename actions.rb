5b9601d236b1f2631e3cd46756b2c4cf10e7fd79
Romil Bhagat <romil@limepoint.com>
2025-10-16 22:19:32 +1100

    draft

--- actions.rb
+++ actions.rb
@@ -271,20 +271,20 @@ action :print_context do
   puts OpsChain.context.to_yaml
 end
 
-#if OpsChain.context.parents.environment == 'dev1'
-#  action :cleanup_temp_files do
-#    log.info 'Cleaning up temporary files'
-#  end
-#else
-#   log.info 'Not running anything as environment is not dev1'
-#end
-
-
-action :cleanup_temp_files do
-  puts OpsChain.context.to_yaml
-  if OpsChain.context.parents.environment == 'dev1'
+if OpsChain.context.parents.environment.code == 'dev1'
+  action :cleanup_temp_files do
     log.info 'Cleaning up temporary files'
-  else
-    log.info 'not cleaning anything as environment is not dev1'
   end
+else
+   log.info 'Not running anything as environment is not dev1'
 end
+
+
+#action :cleanup_temp_files do
+#  puts OpsChain.context.to_yaml
+#  if OpsChain.context.parents.environment == 'dev1'
+#    log.info 'Cleaning up temporary files'
+#  else
+#    log.info 'not cleaning anything as environment is not dev1'
+#  end
+#end
