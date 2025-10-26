ecf2b3c4e25806fcc2321ad6d67fbb421c100005
Romil Bhagat <romil@limepoint.com>
2025-10-27 09:25:25 +1100

    draft

--- printer/actions.rb
+++ printer/actions.rb
@@ -1,4 +1,4 @@
-action :print_names, description: 'prints names from a file' do
+action :print_names, description: 'prints names from a file', steps: [:print_names_without_gui] do
   exec_command 'cat files/list_of_names.txt'
 end
 
@@ -6,3 +6,4 @@ action :print_names_without_gui do
   log.info 'I will not been seen in the ui but I can be executed'
   log.info 'I can be executed as a step of another action or just directly via run_change'
 end
+
