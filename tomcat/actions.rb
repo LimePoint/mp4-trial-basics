5e7aec0c5abc365f50a79e755077c0bfd0c0c2c5
Romil Bhagat <romil@limepoint.com>
2025-10-28 14:32:39 +1100

    changes

--- tomcat/actions.rb
+++ tomcat/actions.rb
@@ -1,3 +1,3 @@
-action :print_memory, description 'Prints Tomcat Memory' do
+action :print_memory, description: 'Prints Tomcat Memory' do
   log.info "Minimum memory required: #{OpsChain.properties.tomcat.minimum_memory}"
 end
