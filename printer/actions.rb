b02a141e04f3204e47b5d7260431aae1001620e4
Romil Bhagat <romil@limepoint.com>
2025-10-28 11:17:43 +1100

    draft

--- printer/actions.rb
+++ printer/actions.rb
@@ -7,4 +7,4 @@ action :print_names_without_gui do
   log.info 'I can be executed as a step of another action or just directly via run_change'
 end
 
-action :print_all_names, description: 'prints all names', steps: [print_names, print_names_without_gui]
+action :print_all_names, description: 'prints all names', steps: [:print_names, :print_names_without_gui]
