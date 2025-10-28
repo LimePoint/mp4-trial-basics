bc95f87b85013b592fdf392983179f69aa3e07c6
Romil Bhagat <romil@limepoint.com>
2025-10-28 14:39:45 +1100

    cjhanges

--- tomcat/actions.rb
+++ tomcat/actions.rb
@@ -3,7 +3,7 @@
 #end
 
 
-action :print_memory, description: 'prints names from a file' do                                                          
+action :print_memory, description: 'Prints Tomcat Memory' do                                                          
   log.info "somethibng here"
 end
 
