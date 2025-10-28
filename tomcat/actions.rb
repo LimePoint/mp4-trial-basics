62fddcb938170e61b7d71c8a31a4a453c708848e
Romil Bhagat <romil@limepoint.com>
2025-10-28 14:37:53 +1100

    cjhanges

--- tomcat/actions.rb
+++ tomcat/actions.rb
@@ -1,3 +1,9 @@
-action :print_memory, description: 'Prints Tomcat Memory' do
-  log.info "Minimum memory required: #{OpsChain.properties.tomcat.minimum_memory}"
+#action :print_memory, description: 'Prints Tomcat Memory' do
+#  log.info "Minimum memory required: #{OpsChain.properties.tomcat.minimum_memory}"
+#end
+
+
+action :print_memory, description: 'prints names from a file' do                                                          
+  log.info "somethibng here"
 end
+
