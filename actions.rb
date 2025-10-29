3e29c4e4243b88906d4f5140bc78b5c2d0264126
Raviteja Dadi <L141171@westpacgroup.com>
2025-10-29 16:48:05 +1100

    training

--- actions.rb
+++ actions.rb
@@ -6,6 +6,6 @@ action 'hello_world_string' do
   puts "Hello world from MintPress"
 end
 action :run_shell_commands do
-  exec_command '/bin/false'
-  exec_command 'whoami' # this will not run as the previous command will fail
+  exec_command '/bin/false', abort_on_failure: false
+  exec_command 'whoami' # this will run as the previous command result do not matter
 end
