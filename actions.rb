0a9319ad1e7d57e2135f59a04e4a98938d246df5
Deepak Rai <Deepak.Rai@westpac.com.au>
2025-10-29 17:23:42 +1100

    testing4

--- actions.rb
+++ actions.rb
@@ -49,16 +49,36 @@
 #end
 #
 
-my_name = 'foo'
+#my_name = 'foo'
 
 #action :print_variable_1 do
 #  puts "--------- Value of variable my_name: #{my_name}"
 #  puts "--------- Value of variable my_address: #{my_address}"
 #end
 
-my_address = 'bar'
+#my_address = 'bar'
 
-action :print_variable_2 do
+#action :print_variable_2 do
+#  puts "--------- Value of variable my_name: #{my_name}"
+#  puts "--------- Value of variable my_address: #{my_address}"
+#end
+#
+#
+#
+my_name = 'foo'
+my_address = 'foo'
+
+action :print_variable do
   puts "--------- Value of variable my_name: #{my_name}"
   puts "--------- Value of variable my_address: #{my_address}"
 end
+
+#my_address = 'bar'
+
+#action :print_variable_3 do
+#  my_address = 'reset'
+#  puts "--------- Value of variable my_name: #{my_name}"
+#  puts "--------- Value of variable my_address: #{my_address}"
+#end
+
+
