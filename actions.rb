fa9def65b5ff778c66333bf8080d6bd8804bf3fe
Satish <satish.thiruvaipati@westpac.com.au>
2025-10-29 17:45:56 +1100

    Updated_string13 scripts.rb

--- actions.rb
+++ actions.rb
@@ -1,3 +1,3 @@
-action :parent_1, steps: [:child_1, :child_2] do
-    log.info "I am parent action 1 and will now call child_1 and child_2 one by one"
+action :grandparent_1, steps: [:parent_1, :parent_2] do
+    log.info "I am grandparent 1"
   end
