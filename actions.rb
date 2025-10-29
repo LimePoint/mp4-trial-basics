c91e045a274b7098631abf472680d47c9ad33ab2
Satish <satish.thiruvaipati@westpac.com.au>
2025-10-29 16:48:16 +1100

    Updated_string3 actions.rb

--- actions.rb
+++ actions.rb
@@ -1,4 +1,4 @@
 action :run_shell_commands do
-    exec_command '/bin/false'
-    exec_command 'whoami' # this will not run as the previous command will fail
+    exec_command '/bin/false', abort_on_failure: false
+    exec_command 'whoami' # this will run as the previous command result do not matter
   end
