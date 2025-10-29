a4481954b5ba482129f53c533a982fb9f02b2f69
Deepak Rai <Deepak.Rai@westpac.com.au>
2025-10-29 16:42:05 +1100

    testing3

--- actions.rb
+++ actions.rb
@@ -10,6 +10,11 @@
 #  puts "Hello world from MintPress"
 #end
 
+#action :run_shell_commands do
+#  exec_command 'whoami'
+#end
+#
 action :run_shell_commands do
   exec_command 'whoami'
+  exec_command 'meminfo'
 end
