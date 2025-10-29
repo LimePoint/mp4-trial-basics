4831ba62b72301b988ae02416b2085af9443794d
Sainath Chenepalle <sainath.chenepalle@westpac.com.au>
2025-10-29 17:38:06 +1100

    Mint_Training

--- actions.rb
+++ actions.rb
@@ -1,9 +1,7 @@
-action :use_logging do                                 
-  puts "Current log level - #{log.level}"      
-  puts "I am being printed via puts"              
-  log.info "I am being printed via the logger as info" 
-  log.warn "I am being printed via the logger as warn" 
-  log.error "I am being printed via the logger as error"
-  log.fatal "I am being printed via the logger as fatal"                                                                                          
-  log.debug "I am being printed via the logger as debug"
-end
+action :print_cpus do                                                                                                                             
+  output = exec_command "bash scripts/get_cpu.sh", live_stream: false
+  cpu = output.stdout
+  log.info "output from get_cpu - #{cpu}"
+  result = exec_command "bash scripts/print_cpu.sh #{cpu}", live_stream: false
+  log.info result.stdout
+end 
---
+++ scripts/print_cpu.sh
@@ -0,0 +1,3 @@
+print_cpu.sh
+cpu_count="$1"
+echo "Number of CPUs: $cpu_count"
