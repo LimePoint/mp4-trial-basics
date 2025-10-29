b1694c82b0d0e382b3992b92d6ef7fb3f275d05d
Santosh Kumar Pillella <L192379@westpacgroup.com>
2025-10-29 18:04:17 +1100

    parent1

--- actions.rb
+++ actions.rb
@@ -1,11 +1,8 @@
-action :child_1 do
-  log.info "I am child action 1, I can be called independently"
+action :grandchild_1 do
+  log.info "I am grand child action 1, I can be called independently. 
+  But when called in prereq mode, I'll run before the calling action"
 end
 
-action :child_2 do
-  log.info "I am child action 2, I can be called independently"
-end
-
-action :parent_1, steps: [:child_1, :child_2] do
-  log.info "I am parent action 1 and will now call child_1 and child_2 one by one"
+action parent_1: [:grandchild_1], steps: [:child_1, :child_2] do
+  log.info "I am parent action 1 and will now call child_1 and child_2 one by one."
 end
