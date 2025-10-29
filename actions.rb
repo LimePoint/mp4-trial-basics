e2336f133aebead9e295d36c8f5033d73ce2ba2c
Deepak Rai <Deepak.Rai@westpac.com.au>
2025-10-29 16:33:12 +1100

    Testing2

--- actions.rb
+++ actions.rb
@@ -1,3 +1,12 @@
+#action :hello_world do
+#  puts "Hello world from MintPress"
+#end
+#
 action :hello_world do
   puts "Hello world from MintPress"
 end
+
+action 'hello_world_string' do
+  puts "Hello world from MintPress"
+end
+
