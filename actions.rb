904882fa7c2a6972cb8b8cf0bff56fa18cae512b
Satish <satish.thiruvaipati@westpac.com.au>
2025-10-29 18:03:38 +1100

    Updated_string15 scripts.rb

--- actions.rb
+++ actions.rb
@@ -1,21 +1,8 @@
-action :child_1 do
-    log.info "I am child action 1, I can be called independently"
+action :grandchild_1 do
+    log.info "I am grand child action 1, I can be called independently. 
+    But when called in prereq mode, I'll run before the calling action"
   end
   
-  action :child_2 do
-    log.info "I am child action 2, I can be called independently"
+  action parent_1: [:grandchild_1], steps: [:child_1, :child_2] do
+    log.info "I am parent action 1 and will now call child_1 and child_2 one by one."
   end
-  
-  action :parent_1, steps: [:child_1, :child_2] do
-    log.info "I am parent action 1 and will now call child_1 and child_2 one by one"
-  end
-  
-  action :parent_2, steps: [:child_1, :child_2], run_as: :parallel do
-    log.info "I am parent action 1 and will now call child_1 and child_2 in parallel."
-  end
-  
-  # complex chaining
-  action :grandparent_1, steps: [:parent_1, :parent_2] do
-    log.info "I am grandparent 1"
-  end
-  
