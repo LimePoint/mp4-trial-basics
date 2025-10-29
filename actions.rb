204c36f193ef729acb9128a55ffae3386fea399e
Raviteja Dadi <L141171@westpacgroup.com>
2025-10-29 17:41:44 +1100

    training

--- actions.rb
+++ actions.rb
@@ -27,3 +27,26 @@ action :print_cpus do
   result = exec_command "bash scripts/print_cpu.sh #{cpu}", live_stream: false
   log.info result.stdout
 end
+
+action :child_1 do
+  log.info "I am child action 1, I can be called independently"
+end
+
+action :child_2 do
+  log.info "I am child action 2, I can be called independently"
+end
+
+action :parent_1, steps: [:child_1, :child_2] do
+  log.info "I am parent action 1 and will now call child_1 and child_2 one by one"
+end
+
+action :parent_2, steps: [:child_1, :child_2], run_as: :parallel do
+  log.info "I am parent action 1 and will now call child_1 and child_2 in parallel."
+end
+
+# complex chaining
+action :grandparent_1, steps: [:parent_1, :parent_2] do
+  log.info "I am grandparent 1"
+end
+
+
