7e419363aa759f5cd1fdf8e023fb678d21129240
Satish <satish.thiruvaipati@westpac.com.au>
2025-10-29 16:39:15 +1100

    Updated_string actions.rb

--- actions.rb
+++ actions.rb
@@ -1,7 +1,3 @@
-action :hello_world do
-    puts "Hello world from MintPress"
-  end
-  
-  action 'hello_world_string' do
-    puts "Hello world from MintPress"
+action :run_shell_commands do
+    exec_command 'whoami'
   end
