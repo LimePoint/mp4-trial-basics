aef6f699d8e8d35bfbf2f582e9ecca23bf7eef0f
Shaneem Mohammed <shaneem.mohammed@westpac.com.au>
2025-10-30 10:03:06 +1100

    test

--- actions.rb
+++ actions.rb
@@ -14,3 +14,29 @@ end
 action :multiple_wait_steps, steps: [OpsChain.wait_step, :post_wait, OpsChain.wait_step, :post_wait_again] do
  log.info "this is multiple wait steps"
 end    
+
+action :child_1 do
+  log.info "I am child action 1, I can be called independently"
+end
+
+action :child_2 do
+  log.info "I am child action 2, I can be called independently"
+end
+
+action :parent_1, steps: [:child_1, :child_2] do
+  log.info "I am parent action 1 and will now call child_1 and child_2 one by one"
+end
+
+action :parent_2, steps: [:child_1, :child_2], run_as: :parallel do
+  log.info "I am parent action 1 and will now call child_1 and child_2 in parallel."
+end
+
+# complex chaining
+action :grandparent_1, steps: [:parent_1, :parent_2] do
+  log.info "I am grandparent 1"
+end
+
+action :run_python do
+  exec_command 'python scripts/sample.py'
+end
+ 
