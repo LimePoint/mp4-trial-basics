34e45585e50f4faf55f42980677688ade86558ac
Deepak Rai <Deepak.Rai@westpac.com.au>
2025-10-29 16:45:12 +1100

    testing4

--- actions.rb
+++ actions.rb
@@ -14,7 +14,12 @@
 #  exec_command 'whoami'
 #end
 #
+#action :run_shell_commands do
+#  exec_command 'whoami'
+#  exec_command 'meminfo'
+#end
+
 action :run_shell_commands do
-  exec_command 'whoami'
-  exec_command 'meminfo'
+  exec_command '/bin/false'
+  exec_command 'whoami' # this will not run as the previous command will fail
 end
