a56957cbf9abe21c401b685f1846f8a064aeeb85
Satish <satish.thiruvaipati@westpac.com.au>
2025-10-29 17:43:26 +1100

    Updated_string12 scripts.rb

--- actions.rb
+++ actions.rb
@@ -1,3 +1,3 @@
-action :child_1 do
-    log.info "I am child action 1, I can be called independently"
+action :parent_1, steps: [:child_1, :child_2] do
+    log.info "I am parent action 1 and will now call child_1 and child_2 one by one"
   end
