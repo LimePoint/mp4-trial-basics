a9b5ad44e339b65f61fd0666f1e37a8c1410e698
Satish <satish.thiruvaipati@westpac.com.au>
2025-10-29 16:43:58 +1100

    Updated_string2 actions.rb

--- actions.rb
+++ actions.rb
@@ -1,4 +1,4 @@
 action :run_shell_commands do
-    exec_command 'whoami'
-    exec_command 'meminfo'
+    exec_command '/bin/false'
+    exec_command 'whoami' # this will not run as the previous command will fail
   end
