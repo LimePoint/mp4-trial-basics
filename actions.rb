14ad40405da296c23cbafa234e9a5ab6bba97a05
Sainath Chenepalle <sainath.chenepalle@westpac.com.au>
2025-10-29 17:14:55 +1100

    Mint_Training

--- actions.rb
+++ actions.rb
@@ -1,13 +1,15 @@
 my_name = 'foo'
+my_address = 'foo'
 
-action :print_variable_1 do
+action :print_variable do
   puts "--------- Value of variable my_name: #{my_name}"
   puts "--------- Value of variable my_address: #{my_address}"
 end
 
 my_address = 'bar'
 
-action :print_variable_2 do
+action :print_variable_3 do
+  my_address = 'reset'
   puts "--------- Value of variable my_name: #{my_name}"
   puts "--------- Value of variable my_address: #{my_address}"
 end
