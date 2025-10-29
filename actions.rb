e9044c9c886eced11c6ae39208b84e7ec81f8151
Satish <satish.thiruvaipati@westpac.com.au>
2025-10-29 16:53:48 +1100

    Updated_string4 actions.rb

--- actions.rb
+++ actions.rb
@@ -1,4 +1,8 @@
 action :run_shell_commands do
-    exec_command '/bin/false', abort_on_failure: false
-    exec_command 'whoami' # this will run as the previous command result do not matter
+    result = exec_command 'cat /etc/passwd'
+    if result.success?
+       puts "found the list of users: #{result.stdout}"
+    else
+       puts "did not find any users."
+    end
   end
