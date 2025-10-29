d44d0357fcee2b0dac7771910442daca0d3931f1
Sahiti Anupapalle <sahiti.anupapalle@westpac.com.au>
2025-10-29 17:38:10 +1100

    Updated actions.rb file

--- actions.rb
+++ actions.rb
@@ -1,7 +1,7 @@
 action :print_cpus do                                                                                                                             
-  output = exec_command "bash scripts/get_cpu.sh", live_stream: false
+  output = exec_command "./scripts/get_cpu.sh", live_stream: false
   cpu = output.stdout
   log.info "output from get_cpu - #{cpu}"
-  result = exec_command "bash scripts/print_cpu.sh #{cpu}", live_stream: false
+  result = exec_command "./scripts/print_cpu.sh #{cpu}", live_stream: false
   log.info result.stdout
 end 
