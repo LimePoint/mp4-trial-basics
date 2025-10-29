c7e98960d67f3579a4f55a409e3896b488a4a0bf
Satish <satish.thiruvaipati@westpac.com.au>
2025-10-29 17:18:17 +1100

    Updated_string8 actions.rb

--- actions.rb
+++ actions.rb
@@ -1,15 +1,3 @@
-my_name = 'foo'
-my_address = 'foo'
-
-action :print_variable do
-  puts "--------- Value of variable my_name: #{my_name}"
-  puts "--------- Value of variable my_address: #{my_address}"
-end
-
-my_address = 'bar'
-
-action :print_variable_3 do
-  my_address = 'reset'
-  puts "--------- Value of variable my_name: #{my_name}"
-  puts "--------- Value of variable my_address: #{my_address}"
-end
+action :find_errors do
+    put "I am missing an s in the puts and will error out"
+  end
