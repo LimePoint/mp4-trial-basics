f319e070f06bbb5d0cc25b35a7b53166e8ad1572
CORPAU\M054575 <m054575@westpacgroup.com>
2025-10-29 16:57:14 +1100

    run_shell_commands_6

--- actions.rb
+++ actions.rb
@@ -33,3 +33,13 @@ action :run_shell_commands_5 do
      puts "did not find any users."
   end
 end
+
+action :run_shell_commands_6 do
+  result = exec_command 'cat /etc/passwd', live_stream: false
+  if result.success?
+     puts "found the list of users: #{result.stdout}"
+  else
+     puts "did not find any users, error was: #{result.stderr}"
+  end
+end
+ 
