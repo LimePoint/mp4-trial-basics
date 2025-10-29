a6d7368f0edb296213a643de07bf2224e3d2e000
Santosh Kumar Pillella <L192379@westpacgroup.com>
2025-10-29 18:02:03 +1100

    parent1

--- actions.rb
+++ actions.rb
@@ -1,3 +1,11 @@
 action :child_1 do
   log.info "I am child action 1, I can be called independently"
 end
+
+action :child_2 do
+  log.info "I am child action 2, I can be called independently"
+end
+
+action :parent_1, steps: [:child_1, :child_2] do
+  log.info "I am parent action 1 and will now call child_1 and child_2 one by one"
+end
