6d93d2bb03535b2dfe1247325c8f5f861acb3518
Srinivas Reddy Muppidi <l201632@westpacgroup.com>
2025-10-29 16:10:43 +1100

    changes

--- printer/actions.rb
+++
@@ -1,9 +0,0 @@
-action :print_names, description: 'prints names from a file', steps: [:print_names_without_gui] do
-  exec_command 'cat files/list_of_names.txt'
-end
-
-action :print_names_without_gui do
-  log.info 'I will not been seen in the ui but I can be executed'
-  log.info 'I can be executed as a step of another action or just directly via run_change'
-end
-
