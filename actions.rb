add73e1b7d14ad78ee53173104978ce2b5629666
Srinivas Reddy Muppidi <l201632@westpacgroup.com>
2025-10-30 09:47:03 +1100

    changes

--- actions.rb
+++ actions.rb
@@ -1,17 +1,4 @@
-action :wait_step, steps: [OpsChain.wait_step, :post_wait] do
-  log.info "This step is going to wait to be manually continuing before running post_wait action"
-end    
-       
-action :post_wait do
- log.info "I will run after the wait step"
-end    
-
-action :post_wait_again do
- log.info "I will also run after a wait step"
-end    
-
-       
-action :multiple_wait_steps, steps: [OpsChain.wait_step, :post_wait, OpsChain.wait_step, :post_wait_again] do
- log.info "this is multiple wait steps"
-end 
+action :run_python do
+  exec_command 'python scripts/sample.py'
+end
 
