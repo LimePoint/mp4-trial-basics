2fc112ecb34b6e7ea564a1c360e79fccb1e935f1
Deepak Rai <Deepak.Rai@westpac.com.au>
2025-10-29 16:01:06 +1100

    Testing

--- tomcat/actions.rb
+++
@@ -1,4 +0,0 @@
-action :print_memory, description: 'Prints Tomcat Memory' do                                                          
-  log.info "Minimum memory required: #{OpsChain.properties.tomcat.minimum_memory}"
-end
-
