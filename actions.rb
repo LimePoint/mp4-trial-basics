da4792a4ec443fe91a8a05beabbf843bff7e7b7b
Satish <satish.thiruvaipati@westpac.com.au>
2025-10-29 17:35:36 +1100

    Updated_string1 cpu.sh

--- actions.rb
+++ actions.rb
@@ -1,9 +1,4 @@
 action :use_logging do                                 
-    puts "Current log level - #{log.level}"      
-    puts "I am being printed via puts"              
-    log.info "I am being printed via the logger as info" 
-    log.warn "I am being printed via the logger as warn" 
-    log.error "I am being printed via the logger as error"
-    log.fatal "I am being printed via the logger as fatal"                                                                                          
-    log.debug "I am being printed via the logger as debug"
-  end  
+        cpu_count=$(grep -c '^processor' /proc/cpuinfo)
+        echo "$cpu_count" 
+  end
