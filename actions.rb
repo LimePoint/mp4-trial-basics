e728ae2c037902354b98ccd07fef8000d2c7968c
Raviteja Dadi <L141171@westpacgroup.com>
2025-10-29 18:05:52 +1100

    training

--- actions.rb
+++ actions.rb
@@ -28,6 +28,23 @@ action :print_cpus do
   log.info result.stdout
 end
 
+action :child_1 do
+  log.info "I am child action 1, I can be called independently"
+end
+
+action :child_2 do
+  log.info "I am child action 2, I can be called independently"
+end
+
+
+action :parent_2, steps: [:child_1, :child_2], run_as: :parallel do
+  log.info "I am parent action 1 and will now call child_1 and child_2 in parallel."
+end
+
+# complex chaining
+action :grandparent_1, steps: [:parent_1, :parent_2] do
+  log.info "I am grandparent 1"
+end
 
 
 action :grandchild_1 do
