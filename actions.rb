84408e5247a7261daf3035ca496f6a73d73b2009
Shaneem Mohammed <shaneem.mohammed@westpac.com.au>
2025-10-29 17:37:13 +1100

    updates 5

--- actions.rb
+++ actions.rb
@@ -1,6 +1,4 @@
 action :cpu_scripts do                                 
-  exec_command 'chmod +x ./scripts/get_cpu.sh'
-  exec_command 'chmod +x ./scripts/print_cpu.sh'
   result1 = exec_command './scripts/get_cpu.sh'
-  exec_command "./scripts/print_cpu.sh #{result1}"
+  exec_command "./scripts/print_cpu.sh #{result1.stdout.strip}"
 end 
