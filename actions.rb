983cb292127cd4f92c5dd7316d0dd8cbe639ee60
CORPAU\M054575 <m054575@westpacgroup.com>
2025-10-29 16:43:50 +1100

    run_shell_commands_3

--- actions.rb
+++ actions.rb
@@ -14,3 +14,8 @@ action :run_shell_commands_2 do
   exec_command 'whoami'
   exec_command 'meminfo'
 end
+
+action :run_shell_commands_3 do
+  exec_command '/bin/false'
+  exec_command 'whoami' # this will not run as the previous command will fail
+end
