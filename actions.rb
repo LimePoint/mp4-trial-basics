eb1c9e7f84177078499bd43d4b57d0468b7deca1
Sahiti Anupapalle <sahiti.anupapalle@westpac.com.au>
2025-10-29 17:28:16 +1100

    Updated actions.rb file

--- actions.rb
+++ actions.rb
@@ -1,9 +1,8 @@
-action :use_logging do                                 
-  puts "Current log level - #{log.level}"      
-  puts "I am being printed via puts"              
-  log.info "I am being printed via the logger as info" 
-  log.warn "I am being printed via the logger as warn" 
-  log.error "I am being printed via the logger as error"
-  log.fatal "I am being printed via the logger as fatal"                                                                                          
-  log.debug "I am being printed via the logger as debug"
-end  
+action :get_cpu do
+  result = exec_command './get_cpu.sh', live_stream: false
+  if result.success?
+     puts "found the list of users: #{result.stdout}"
+  else
+     puts "did not find any users, error was: #{result.stderr}"
+  end
+end
