f28ce6c91901542ae42e0002dd3ab4c38debd9c1
Raviteja Dadi <L141171@westpacgroup.com>
2025-10-29 17:18:09 +1100

    training

--- actions.rb
+++ actions.rb
@@ -6,18 +6,6 @@ action 'hello_world_string' do
   puts "Hello world from MintPress"
 end
 
-my_name = 'foo'
-my_address = 'foo'
-
-action :print_variable do
-  puts "--------- Value of variable my_name: #{my_name}"
-  puts "--------- Value of variable my_address: #{my_address}"
-end
-
-my_address = 'bar'
-
-action :print_variable_3 do
-  my_address = 'reset'
-  puts "--------- Value of variable my_name: #{my_name}"
-  puts "--------- Value of variable my_address: #{my_address}"
+action :find_errors do
+  put "I am missing an s in the puts and will error out"
 end
