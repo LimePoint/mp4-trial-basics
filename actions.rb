a9e8811757b29690ef524be4c7eb7054c13f5650
Sahiti Anupapalle <sahiti.anupapalle@westpac.com.au>
2025-10-29 17:34:23 +1100

    Updated actions.rb file

--- actions.rb
+++ actions.rb
@@ -1,9 +1,7 @@
-action :get_cpu do
-    script_path = File.join(__dir__, 'scripts', 'get_cpu.sh')
-    result = exec_command script_path
-    if result.success?
-         puts "found the list of users: #{result.stdout}"
-    else
-         puts "did not find any users, error was: #{result.stderr}"
-    end
-end
+action :print_cpus do                                                                                                                             
+  output = exec_command "bash scripts/get_cpu.sh", live_stream: false
+  cpu = output.stdout
+  log.info "output from get_cpu - #{cpu}"
+  result = exec_command "bash scripts/print_cpu.sh #{cpu}", live_stream: false
+  log.info result.stdout
+end 
