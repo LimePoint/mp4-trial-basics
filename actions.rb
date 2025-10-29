96533610ea6ba246709f89eb74e70391528be00f
Satish <satish.thiruvaipati@westpac.com.au>
2025-10-29 16:56:02 +1100

    Updated_string5 actions.rb

--- actions.rb
+++ actions.rb
@@ -1,8 +1,8 @@
 action :run_shell_commands do
-    result = exec_command 'cat /etc/passwd'
+    result = exec_command 'cat /etc/passwd', live_stream: false
     if result.success?
        puts "found the list of users: #{result.stdout}"
     else
-       puts "did not find any users."
+       puts "did not find any users, error was: #{result.stderr}"
     end
   end
