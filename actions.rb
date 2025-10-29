af3fa305f88521bdda22b5828fe7af83f3a7b4d1
Deepak Rai <Deepak.Rai@westpac.com.au>
2025-10-29 17:09:23 +1100

    testing4

--- actions.rb
+++ actions.rb
@@ -39,11 +39,26 @@
 #end
 #
 #
-action :run_shell_commands do
-  result = exec_command 'cat /etc/passwd', live_stream: false
-  if result.success?
-     puts "found the list of users: #{result.stdout}"
-  else
-     puts "did not find any users, error was: #{result.stderr}"
-  end
+#action :run_shell_commands do
+#  result = exec_command 'cat /etc/passwd', live_stream: false
+#  if result.success?
+#     puts "found the list of users: #{result.stdout}"
+#  else
+#     puts "did not find any users, error was: #{result.stderr}"
+#  end
+#end
+#
+
+my_name = 'foo'
+
+action :print_variable_1 do
+  puts "--------- Value of variable my_name: #{my_name}"
+  puts "--------- Value of variable my_address: #{my_address}"
+end
+
+my_address = 'bar'
+
+action :print_variable_2 do
+  puts "--------- Value of variable my_name: #{my_name}"
+  puts "--------- Value of variable my_address: #{my_address}"
 end
