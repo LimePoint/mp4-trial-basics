7f5c1b6e2803f27a8a71d03cf6ea9100699292c4
Raviteja Dadi <L141171@westpacgroup.com>
2025-10-29 17:14:25 +1100

    training

--- actions.rb
+++ actions.rb
@@ -5,16 +5,19 @@ end
 action 'hello_world_string' do
   puts "Hello world from MintPress"
 end
-my_name = 'foo'
 
-action :print_variable_1 do
+my_name = 'foo'
+my_address = 'foo'
+
+action :print_variable do
   puts "--------- Value of variable my_name: #{my_name}"
   puts "--------- Value of variable my_address: #{my_address}"
 end
 
 my_address = 'bar'
 
-action :print_variable_2 do
+action :print_variable_3 do
+  my_address = 'reset'
   puts "--------- Value of variable my_name: #{my_name}"
   puts "--------- Value of variable my_address: #{my_address}"
 end
