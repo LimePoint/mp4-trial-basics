93ace672bd0beff4eee3fcb7bdceaac5f34e01fd
Sahiti Anupapalle <sahiti.anupapalle@westpac.com.au>
2025-10-29 16:44:52 +1100

    Updated actions.rb file

--- actions.rb
+++ actions.rb
@@ -1,13 +1,4 @@
 action :run_shell_commands do
   exec_command 'whoami'
-end
-
-action :run_shell_commands do
-  exec_command 'whoami'
   exec_command 'meminfo'
-end
-
-action :run_shell_commands do
-  exec_command '/bin/false'
-  exec_command 'whoami' # this will not run as the previous command will fail
 end
