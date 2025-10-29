dca182fbcaa416fe832d322f4ccfa5c44f3eec85
Raviteja Dadi <L141171@westpacgroup.com>
2025-10-29 16:58:16 +1100

    training

--- actions.rb
+++ actions.rb
@@ -6,7 +6,7 @@ action 'hello_world_string' do
   puts "Hello world from MintPress"
 end
 action :run_shell_commands do
-  result = exec_command 'cat /etc/passwd', live_stream: false
+  result = exec_command 'cat /etc/passw', live_stream: false
   if result.success?
      puts "found the list of users: #{result.stdout}"
   else
