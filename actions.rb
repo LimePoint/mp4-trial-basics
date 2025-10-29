ba79874c73084553781b53a8a0e324fc2ffe2e98
CORPAU\M054575 <m054575@westpacgroup.com>
2025-10-29 17:32:33 +1100

    example1

--- actions.rb
+++ actions.rb
@@ -75,7 +75,7 @@ action :run_shell_commands_ex1 do
   result = exec_command 'scripts/get_cpu.sh', live_stream: false
   if result.success?
      puts "found the list of users: #{result.stdout}"
-     exec_command 'scripts/print_cpu.sh', result.stdout
+     result1 = exec_command 'scripts/print_cpu.sh', result1.stdout
   else
      puts "did not find any users, error was: #{result.stderr}"
   end
