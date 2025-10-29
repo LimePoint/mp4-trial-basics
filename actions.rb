483cb8ba3392d08695242a0d3fc1fecc0547d21e
Shaneem Mohammed <shaneem.mohammed@westpac.com.au>
2025-10-29 17:29:29 +1100

    updates 5

--- actions.rb
+++ actions.rb
@@ -1,9 +1,6 @@
-action :use_logging do                                 
-  puts "Current log level - #{log.level}"      
-  puts "I am being printed via puts"              
-  log.info "I am being printed via the logger as info" 
-  log.warn "I am being printed via the logger as warn" 
-  log.error "I am being printed via the logger as error"
-  log.fatal "I am being printed via the logger as fatal"                                                                                          
-  log.debug "I am being printed via the logger as debug"
+action :cpu_scripts do                                 
+  exec_command 'chmod +x ./scripts/get_cpu.sh'
+  exec_command 'chmod +x ./scripts/print_cpu.sh'
+  exec_command './scripts/get_cpu.sh'
+  exec_command './scripts/print_cpu.sh'
 end 
