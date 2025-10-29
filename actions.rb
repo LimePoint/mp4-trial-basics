297a2426beacecb75bf33afdc66ca77ba3de35ee
Santosh Kumar Pillella <L192379@westpacgroup.com>
2025-10-29 18:00:11 +1100

    child1

--- actions.rb
+++ actions.rb
@@ -1,19 +1,3 @@
 action :child_1 do
   log.info "I am child action 1, I can be called independently"
 end
-
-action :child_2 do
-  log.info "I am child action 2, I can be called independently"
-end
-
-action :parent_1, steps: [:child_1, :child_2] do
-  log.info "I am parent action 1 and will now call child_1 and child_2 one by one"
-end
-
-action :parent_2, steps: [:child_1, :child_2], run_as: :parallel do
-  log.info "I am parent action 1 and will now call child_1 and child_2 in parallel."
-end
-
-# complex chaining
-action :grandparent_1, steps: [:parent_1, :parent_2] do
-  log.info "I am grandparent 1"
