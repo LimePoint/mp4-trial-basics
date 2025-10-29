451ceb2373370c176331ae3a31f7c59dcebcb217
Shaneem Mohammed <shaneem.mohammed@westpac.com.au>
2025-10-29 17:42:22 +1100

    updates

--- actions.rb
+++ actions.rb
@@ -1,4 +1,21 @@
-action :cpu_scripts do                                 
-  result1 = exec_command './scripts/get_cpu.sh'
-  exec_command "./scripts/print_cpu.sh #{result1.stdout.strip}"
-end 
+action :child_1 do
+  log.info "I am child action 1, I can be called independently"
+end
+
+action :child_2 do
+  log.info "I am child action 2, I can be called independently"
+end
+
+action :parent_1, steps: [:child_1, :child_2] do
+  log.info "I am parent action 1 and will now call child_1 and child_2 one by one"
+end
+
+action :parent_2, steps: [:child_1, :child_2], run_as: :parallel do
+  log.info "I am parent action 1 and will now call child_1 and child_2 in parallel."
+end
+
+# complex chaining
+action :grandparent_1, steps: [:parent_1, :parent_2] do
+  log.info "I am grandparent 1"
+end
+ 
