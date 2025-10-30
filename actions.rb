f420852d9ce4cc5fa261c208b45d659cdabc11d0
Min Gu <M052909@westpacgroup.com>
2025-10-30 16:12:04 +1100

    added shell scripts

--- actions.rb
+++ actions.rb
@@ -70,3 +70,13 @@ end
 action :find_errors do
   put "I am missing an s in the puts and will error out"
 end
+
+action :use_logging do                                 
+  puts "Current log level - #{log.level}"      
+  puts "I am being printed via puts"              
+  log.info "I am being printed via the logger as info" 
+  log.warn "I am being printed via the logger as warn" 
+  log.error "I am being printed via the logger as error"
+  log.fatal "I am being printed via the logger as fatal"                                                                                          
+  log.debug "I am being printed via the logger as debug"
+end    
