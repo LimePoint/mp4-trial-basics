aba80892cc4d468be611316b2cd150e08213914f
Neha Vashisht <neha.vashisht1@westpac.com.au>
2025-10-30 16:23:12 +1100

    action data

--- actions.rb
+++ actions.rb
@@ -70,3 +70,17 @@ action :print_variable do
   action :find_errors do
 	put "I am missing an s in the puts and will error out"
   end
+
+  action :use_logging do                                 
+	puts "Current log level - #{log.level}"      
+	puts "I am being printed via puts"              
+	log.info "I am being printed via the logger as info" 
+	log.warn "I am being printed via the logger as warn" 
+	log.error "I am being printed via the logger as error"
+	log.fatal "I am being printed via the logger as fatal"                                                                                          
+	log.debug "I am being printed via the logger as debug"
+  end 
+
+  action : script_call do
+	exec_command scripts/get_cpu.sh
+  end
