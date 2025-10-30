9728f6739001ff94657382817783255372d8ab97
PAVAN KALYAN BOYA <L159773@westpacgroup.com>
2025-10-30 15:57:06 +1100

    added multiple actions

--- actions.rb
+++ actions.rb
@@ -36,3 +36,17 @@ action :run_shell_commands_7 do
      puts "did not find any users, error was: #{result.stderr}"
   end
 end
+
+my_name = 'foo'
+
+action :print_variable_1 do
+  puts "--------- Value of variable my_name: #{my_name}"
+  puts "--------- Value of variable my_address: #{my_address}"
+end
+
+my_address = 'bar'
+
+action :print_variable_2 do
+  puts "--------- Value of variable my_name: #{my_name}"
+  puts "--------- Value of variable my_address: #{my_address}"
+end
