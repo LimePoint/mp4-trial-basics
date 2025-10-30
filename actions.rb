9acc4cf0a60f53e8ef77292130e1b28c92d9964f
Neha Vashisht <neha.vashisht1@westpac.com.au>
2025-10-30 16:24:24 +1100

    action data

--- actions.rb
+++ actions.rb
@@ -61,11 +61,6 @@ end
 action :print_variable do
 	puts "I am the first line"
   end
-  
-  
-  action :	 do
-	puts "I am the second line"
-  end
 
   action :find_errors do
 	put "I am missing an s in the puts and will error out"
