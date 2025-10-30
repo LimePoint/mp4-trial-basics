6fd83db2d2cff59fb375412c53194d3722b27008
Neha Vashisht <neha.vashisht1@westpac.com.au>
2025-10-30 17:02:15 +1100

    action data

--- actions.rb
+++ actions.rb
@@ -20,6 +20,15 @@ action :child_1 do
 	log.info "I am grandparent 1"
   end
 
+  action :grandchild_1 do
+	log.info "I am grand child action 1, I can be called independently. 
+	But when called in prereq mode, I'll run before the calling action"
+  end
+  
+  action parent_3: [:grandchild_1], steps: [:child_1, :child_2] do
+	log.info "I am parent action 1 and will now call child_1 and child_2 one by one."
+  end
+  
   action :script_call do
 	exec_command "bash scripts/get_cpu.sh"
   end
