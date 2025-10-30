46db264ee995c36883727b38a4f2c76340e02258
Min Gu <M052909@westpacgroup.com>
2025-10-30 15:43:57 +1100

    added shell scripts

--- actions.rb
+++ actions.rb
@@ -20,3 +20,8 @@ action :run_shell_commands_4 do
   exec_command '/bin/false', abort_on_failure: false
   exec_command 'whoami' # this will run as the previous command result do not matter
 end
+
+action :run_shell_commands_5 do
+  exec_command '/tmp/non_existent_script.sh'
+  puts “I will never get run”
+end
