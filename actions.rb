3bb370dbae16538a2f18f83a6f9e0396b9c45423
Naveen Yedla <naveen.yedla@westpac.com.au>
2025-10-30 16:07:19 +1100

    updted file

--- actions.rb
+++ actions.rb
@@ -68,3 +68,7 @@ action :print_variable do
   action :print_variable do
     puts "I am the second line"
   end
+
+  action :find_errors do
+    put "I am missing an s in the puts and will error out"
+  end
