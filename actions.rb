dca9eec32b7262e554ebcf05af1d3336661084a3
Jyoti Mehra <L178242@westpacgroup.com>
2025-10-30 16:08:37 +1100

    Action file modified

--- actions.rb
+++ actions.rb
@@ -62,7 +62,10 @@ action :print_variable do
   puts "I am the first line"
 end
 
-
 action :print_variable do
   puts "I am the second line"
+end
+
+action :find_errors do
+  put "I am missing an s in the puts and will error out"
 end
