c6c2ef8aac55bbf96a5556ce94ea44bcee024b4d
Neha Vashisht <neha.vashisht1@westpac.com.au>
2025-10-30 15:21:58 +1100

    action data

--- actions.rb
+++ actions.rb
@@ -1,3 +1,17 @@
 action :hello_world do
 	puts "Hello Worl"
 end
+
+action :run_shell_commands do
+	exec_command 'whoami'
+  end
+
+  action :run_shell_commands_2 do
+	exec_command 'whoami'
+	exec_command 'meminfo'
+  end
+
+  action :run_shell_commands_3 do
+	exec_command '/bin/false'
+	exec_command 'whoami' # this will not run as the previous command will fail
+  end
