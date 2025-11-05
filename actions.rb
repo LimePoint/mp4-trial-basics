3297818a76d4aeb76ae59296386fd698db183bba
Vivek Gupta <M055958@westpacgroup.com>
2025-11-05 16:58:30 +1100

    added for vivek

--- actions.rb
+++ actions.rb
@@ -120,11 +120,13 @@ action :use_logging2 do
 end
 
 action :passVariable1 do
-  result = exec_command 'chmod +x scripts/get_cpu.sh;scripts/get_cpu.sh'
+  exec_command 'chmod +x scripts/get_cpu.sh'
+  exec_command 'chmod +x scripts/get_cpu.sh'
+  result = exec_command 'scripts/print_cpu.sh'
   if result.success?
     cpuno = result.stdout
     log.info "I am being printed via the logger as info #{cpuno}" 
-    exec_command "chmod +x scripts/print_cpu.sh;scripts/print_cpu.sh #{result.stdout}"
+    exec_command "scripts/print_cpu.sh #{result.stdout}"
  else
     puts "Did not find result"
  end
