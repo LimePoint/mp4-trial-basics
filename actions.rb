3cf6a2df5bf2708676d017a654a2224dbf8f6607
Vivek Gupta <M055958@westpacgroup.com>
2025-11-05 21:18:14 +1100

    added for vivek

--- actions.rb
+++ actions.rb
@@ -222,6 +222,12 @@ action :run_python do
  action :kv_properties do 
   log.info "Total number of employees: #{OpsChain.properties.to_yaml}"
  end
+
+ action :kv_properties do 
+  log.info "First employee: 
+ #{OpsChain.properties.organization.employees[0].name}" 
+ end
+ 
  
  
 
