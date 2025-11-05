4319ae9e624c3be591e83917bf4d207cc0a6f945
Vivek Gupta <M055958@westpacgroup.com>
2025-11-05 17:31:17 +1100

    added for vivek

--- actions.rb
+++ actions.rb
@@ -149,7 +149,7 @@ action :parent_1, steps: [:child_1, :child_2] do
   log.info "I am parent action 1 and will now call child_1 and child_2 one by one"
 end
 
-action :parent_2, steps: [:child_1, :child_2, child_3], run_as: :parallel do
+action :parent_2, steps: [:child_1, :child_2, :child_3], run_as: :parallel do
   log.info "I am parent action 1 and will now call child_1 and child_2 in parallel."
 end
 
