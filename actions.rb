0013bf6cc3b57d30a8362ed3e119a5ba7ffe5493
Vivek Gupta <M055958@westpacgroup.com>
2025-11-05 18:59:54 +1100

    added for vivek

--- actions.rb
+++ actions.rb
@@ -191,9 +191,19 @@ action :run_python do
  action :download_rates_file do
   exec_command 'curl https://foo.com/rates.json -o rates.json'
  end
- 
+
  action :update_rates, steps: [:download_rates] do
   exec_command 'curl -X POST -d @rates.json'
+ end
+
+ action :pod_step_read do
+  log.info "Trying to read file /tmp/file1.txt create in write step"
+  log.info File.read('/tmp/file1.txt') 
+ end 
+  
+ action :pod_step_write, steps: [:pod_step_read] do
+  File.write('/tmp/file1.txt', "something")
+  exec_command "ls -lrth /tmp"
  end
  
  
