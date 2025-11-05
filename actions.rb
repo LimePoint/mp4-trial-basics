2e3154ac51cb85113b7f3181d8770e0e515e7d83
Vineet Sinha <vineet.sinha@westpac.com.au>
2025-11-05 15:54:28 +1100

    Fifth commit

--- actions.rb
+++ actions.rb
@@ -45,3 +45,16 @@ action :run_shell_commands_7 do
   end
 end
 
+my_name = 'foo'
+
+action :print_variable_1 do
+  puts "--------- Value of variable my_name: #{my_name}"
+  puts "--------- Value of variable my_address: #{my_address}"
+end
+
+my_address = 'bar'
+
+action :print_variable_2 do
+  puts "--------- Value of variable my_name: #{my_name}"
+  puts "--------- Value of variable my_address: #{my_address}"
+end
