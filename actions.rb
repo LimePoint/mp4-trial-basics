8dae9cb5e82bec6a556d133aa6df77b4c4a1fd7c
Vivek Gupta <M055958@westpacgroup.com>
2025-11-05 22:19:32 +1100

    added for vivek

--- actions.rb
+++ actions.rb
@@ -231,6 +231,12 @@ action :run_python do
  action :file_properties do
   log.info "Contents of file at /tmp/file1.txt - #{File.read('/tmp/file1.txt')}"
  end
+
+ action :env_properties do
+  log.info "Value of MY_VARIABLE: #{ENV['MY_VARIABLE']}"
+  log.info "Value of ANOTHER_VARIABLE: #{ENV['ANOTHER_VARIABLE']}"
+ end 
+ 
  
  
  
