e16243ce162fc43b6b4130c8016ec1ad0927ea50
Vivek Gupta <M055958@westpacgroup.com>
2025-11-05 16:18:06 +1100

    added for vivek

--- actions.rb
+++ actions.rb
@@ -95,7 +95,18 @@ action :use_logging do
   log.error "I am being printed via the logger as error"
   log.fatal "I am being printed via the logger as fatal"                                                                                          
   log.debug "I am being printed via the logger as debug"
-end          
+end  
+
+action :use_logging1 do 
+  log.level = ::Logger::ERROR                                
+  puts "Current log level - #{log.level}"      
+  puts "I am being printed via puts"              
+  log.info "I am being printed via the logger as info" 
+  log.warn "I am being printed via the logger as warn" 
+  log.error "I am being printed via the logger as error"
+  log.fatal "I am being printed via the logger as fatal"                                                                                          
+  log.debug "I am being printed via the logger as debug"
+end 
  
  
  
