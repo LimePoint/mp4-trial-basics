92a63dc0528b94954a480428817e40571a463eb4
Vineet Sinha <vineet.sinha@westpac.com.au>
2025-11-05 14:33:08 +1100

    Second commit

--- actions.rb
+++ actions.rb
@@ -1,3 +1,17 @@
 action :hello_world do
   puts "Hello world from MintPress"
 end
+
+action :run_shell_commands do
+  exec_command 'whoami'
+end
+
+action :run_shell_commands_2 do
+  exec_command 'whoami'
+  exec_command 'meminfo'
+end
+
+action :run_shell_commands_3 do
+  exec_command '/bin/false'
+  exec
+end
