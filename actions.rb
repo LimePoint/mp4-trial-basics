1761623a4aa954103846115c855683ef048c99b7
Vineet Sinha <vineet.sinha@westpac.com.au>
2025-11-05 15:59:06 +1100

    Sixth commit

--- actions.rb
+++ actions.rb
@@ -58,3 +58,19 @@ action :print_variable_2 do
   puts "--------- Value of variable my_name: #{my_name}"
   puts "--------- Value of variable my_address: #{my_address}"
 end
+
+my_name = 'foo'
+my_address = 'foo'
+
+action :print_variable do
+  puts "--------- Value of variable my_name: #{my_name}"
+  puts "--------- Value of variable my_address: #{my_address}"
+end
+
+my_address = 'bar'
+
+action :print_variable_3 do
+  my_address = 'reset'
+  puts "--------- Value of variable my_name: #{my_name}"
+  puts "--------- Value of variable my_address: #{my_address}"
+end
