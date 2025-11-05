84568db74b6585f1497b81bdafd63e0aac2ef6db
Vineet Sinha <vineet.sinha@westpac.com.au>
2025-11-05 14:42:05 +1100

    Third commit

--- actions.rb
+++ actions.rb
@@ -13,5 +13,15 @@ end
 
 action :run_shell_commands_3 do
   exec_command '/bin/false'
-  exec
+  exec 'whoami'
+end
+
+action :run_shell_commands_4 do
+  exec_command '/bin/false', abort_on_failure: false
+  exec_command 'whoami' # this will run as the previous command result do not matter
+end
+
+action :run_shell_commands_5 do
+  exec_command '/tmp/non_existent_script.sh'
+  puts “I will never get run”
 end
