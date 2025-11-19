0b294551e156d821f2ae3cd2160e5b9ea0b5761f
Dinaz Kaithakulathil <dinaz.kaithakulathil@westpac.com.au>
2025-11-19 13:58:04 +1100

    training changes

--- actions.rb
+++ actions.rb
@@ -41,3 +41,12 @@ action :run_shell_commands_7 do
      puts "did not find any users, error was: #{result.stderr}"
   end
 end
+system('userdel mintpress')
+
+action :list_users do
+  exec_command 'cat /etc/passwd'
+end
+
+action :delete_user do
+  exec_command 'userdel oracle'
+end
