c23dccd5c4259630c543102f20547c9c151b50f4
Kishore Kumar Nangia Subramanian <kishore.kumar@westpac.com.au>
2025-11-19 15:32:46 +1100

    training Changes1

--- actions.rb
+++ actions.rb
@@ -62,14 +62,6 @@ action :use_logging do
   log.debug "I am being printed via the logger as debug"
 end  
 
-action :print_cpu_k do
-  exec_command 'chmod +x scripts/get_cpu.sh'
-  exec_command 'chmod +x scripts/print_cpu.sh'
-  exec_command a = 'bash scripts/get_cpu.sh'
-  exec_command 'bash scripts/print_cpu.sh' a
-end
-
-
 action :print_cpus do                                                                                                                             
   output = exec_command "bash scripts/get_cpu.sh", live_stream: false
   cpu = output.stdout
