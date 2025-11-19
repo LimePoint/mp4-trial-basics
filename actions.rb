73d28ed3085a1a4f10ae846cefa71972ba20077d
Philip Knowles <M044147@westpacgroup.com>
2025-11-19 15:31:08 +1100

    Add cpu solution action

--- actions.rb
+++ actions.rb
@@ -71,3 +71,11 @@ action :get_cpu_number do
   result = exec_command 'bash scripts/get_cpu.sh', live_stream: false
   exec_command "bash scripts/print_cpu.sh #{result.stdout}"
 end
+
+action :print_cpus do
+  output = exec_command "bash scripts/get_cpu.sh", live_stream: false
+  cpu = output.stdout
+  log.info "output from get_cpu - #{cpu}"
+  result = exec_command "bash scripts/print_cpu.sh #{cpu}", live_stream: false
+  log.info result.stdout
+end
