cb09118689bfa258a8032257ccc02a1edb9a0768
Nagesh Balasubramanian <nagesh.balasubramanian@westpac.com.au>
2025-11-19 15:20:55 +1100

    action change 1

--- actions.rb
+++ actions.rb
@@ -64,6 +64,6 @@ action :print_cpu do
   cpu_output = exec_command "bash scripts/get_cpu.sh"
   getcpu = cpu_output.stdout
   log_info "cpu details - #{getcpu}"
-  output = exec_command "bash scripts/print_cpu.sh #{getcpu}"
+  output = exec_command "bash scripts/print_cpu.sh #{cpu_output}"
   log.info output.stdout
 end
