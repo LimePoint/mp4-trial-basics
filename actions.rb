fd187d1dbf68c9ebfada00ff189d602237b8e54d
Kishore Kumar Nangia Subramanian <kishore.kumar@westpac.com.au>
2025-11-19 13:56:41 +1100

    training Changes1

--- actions.rb
+++ actions.rb
@@ -41,4 +41,10 @@ action :run_shell_commands_7 do
      puts "did not find any users, error was: #{result.stderr}"
   end
 end
+action :list_users do
+  exec_command 'cat /etc/passwd'
+end
 
+action :delete_user do
+  exec_command 'userdel oracle'
+end
