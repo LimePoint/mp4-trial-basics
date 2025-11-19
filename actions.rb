c86eae3710100deed7d386924c997837726fbab6
Kishore Kumar Nangia Subramanian <kishore.kumar@westpac.com.au>
2025-11-19 15:37:45 +1100

    training Changes1

--- actions.rb
+++ actions.rb
@@ -74,5 +74,5 @@ action :print_cpu_k do
   exec_command 'chmod +x scripts/get_cpu.sh'
   exec_command 'chmod +x scripts/print_cpu.sh'
   cpu = exec_command 'bash scripts/get_cpu.sh'
-  result = exec_command 'bash scripts/print_cpu.sh' {cpu}
+  result = exec_command "bash scripts/print_cpu.sh #{cpu}"
 end
