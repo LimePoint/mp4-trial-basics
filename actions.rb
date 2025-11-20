1d2b79c90544c40525cabba1025b95905cc22388
Sahiti Anupapalle <sahiti.anupapalle@westpac.com.au>
2025-11-20 11:08:42 +1100

    Added properties

--- actions.rb
+++ actions.rb
@@ -48,3 +48,13 @@ end
 action :run_python do
   exec_command 'python scripts/sample.py'
 end
+
+action :kv_properties do                                     
+  log.info "Total number of employees: #{OpsChain.properties.to_yaml}"
+end
+ 
+
+action :kv_properties do                                     
+  log.info "First employee: #{OpsChain.properties.organization.employees[0].name}" 
+end
+ 
