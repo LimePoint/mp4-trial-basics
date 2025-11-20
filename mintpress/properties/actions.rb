0cc723983a55cce2aa13265de51e5cf3b8f9f7e8
Vineet Sinha <vineet.sinha@westpac.com.au>
2025-11-20 11:11:24 +1100

    push

---
+++ mintpress/properties/actions.rb
@@ -0,0 +1,8 @@
+action :kv_properties do                                     
+  log.info "Total number of employees: #{OpsChain.properties.to_yaml}"
+end
+
+action :kv_properties do
+  log.info "First employee: #{OpsChain.properties.organization.employees[0].name}"
+end
+
