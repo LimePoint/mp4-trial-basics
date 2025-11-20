65fcaa5808880d59ad59acf02aefeab26040da9d
Jyoti Mehra <L178242@westpacgroup.com>
2025-11-20 11:26:42 +1100

    properties file

--- actions.rb
+++ actions.rb
@@ -159,3 +159,6 @@ action :kv_properties do
   log.info "First employee: #{OpsChain.properties.organization.employees[0].name}" 
 end
 
+action :file_properties do
+  exec_command "cat /tmp/file1.txt"
+end
