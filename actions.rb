70143d631920ba4ef6cdd817b779d61869afadbd
Min Gu <M052909@westpacgroup.com>
2025-11-20 11:09:04 +1100

    updated for properties

--- actions.rb
+++ actions.rb
@@ -152,3 +152,10 @@ action :cleanup_temp_files do
   end                                         
 end
  
+action :kv_properties do                                     
+  log.info "Total number of employees: #{OpsChain.properties.to_yaml}"
+  log.info "First employee: #{OpsChain.properties.organization.employees[0].name}" 
+end
+ 
+
+ 
