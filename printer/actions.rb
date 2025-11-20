3d2b58491afdd9db7f73196198a56fcb3a8b3b14
Vivek Gupta <M055958@westpacgroup.com>
2025-11-20 16:07:23 +1100

    adding template

--- printer/actions.rb
+++ printer/actions.rb
@@ -8,4 +8,8 @@ action :print_names, description: 'prints names from a file' do
  action :print_all_names, description: 'prints all names', steps: [:print_names, :print_names_without_gui] do 
   exec_command 'cat files/list_of_names.txt'
  end
+
+ action :print_names_again, description: 'prints again' do                                                          
+  log.info "again printing name"
+end
  
