614e760a805e5fb9a59bb69dd0f131d4640c0f7d
Vineet Sinha <vineet.sinha@westpac.com.au>
2025-11-20 13:20:27 +1100

    change

--- actions.rb
+++ actions.rb
@@ -151,3 +151,25 @@ end
 action :multiple_wait_steps, steps: [OpsChain.wait_step, :post_wait, OpsChain.wait_step, :post_wait_again] do
  log.info "this is multiple wait steps"
 end
+
+action :kv_properties do
+  log.info "Total number of employees: #{OpsChain.properties.to_yaml}"
+end
+
+action :kv_properties do
+  log.info "First employee: #{OpsChain.properties.organization.employees[0].name}"
+end
+
+action :file_properties do
+  log.info "Contents of file at /tmp/file1.txt - #{File.read('/tmp/file1.txt')}"
+end
+
+action :file_properties do
+  exec_command "cat /tmp/file1.txt"
+end
+
+action :env_properties do
+  log.info "Value of MY_VARIABLE: #{ENV['MY_VARIABLE']}"
+  log.info "Value of ANOTHER_VARIABLE: #{ENV['ANOTHER_VARIABLE']}"
+end
+
