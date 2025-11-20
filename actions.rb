e3825fe2367b22161f3a067819b8589d82492693
Vivek Gupta <M055958@westpacgroup.com>
2025-11-20 11:23:21 +1100

    second way of file params

--- actions.rb
+++ actions.rb
@@ -232,6 +232,10 @@ action :run_python do
   log.info "Contents of file at /tmp/file1.txt - #{File.read('/tmp/file1.txt')}"
  end
 
+ action :file_properties do
+  exec_command "cat /tmp/file1.txt"
+end
+
  action :env_properties do
   log.info "Value of MY_VARIABLE: #{ENV['MY_VARIABLE']}"
   log.info "Value of ANOTHER_VARIABLE: #{ENV['ANOTHER_VARIABLE']}"
