0b1786b88d357303e80e2ab11c960f18ba54ba9a
Raviteja Dadi <L141171@westpacgroup.com>
2025-11-20 11:10:46 +1100

    training

--- actions.rb
+++ actions.rb
@@ -78,3 +78,10 @@ end
 action :run_python do
   exec_command 'python scripts/sample.py'
 end
+
+action :kv_properties do                                     
+  log.info "Total number of employees: #{OpsChain.properties.to_yaml}"
+end
+action :kv_properties1 do                                     
+  log.info "First employee: #{OpsChain.properties.organization.employees[0].name}" 
+end
