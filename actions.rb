45472bcb71557e0ec41e906e88453449375dfb26
Jyoti Mehra <L178242@westpacgroup.com>
2025-11-20 11:56:23 +1100

    properties file

--- actions.rb
+++ actions.rb
@@ -162,3 +162,8 @@ end
 action :file_properties do
   exec_command "cat /tmp/file1.txt"
 end
+
+action :env_properties do
+  log.info "Value of MY_VARIABLE: #{ENV['MY_VARIABLE']}"
+  log.info "Value of ANOTHER_VARIABLE: #{ENV['ANOTHER_VARIABLE']}"
+end 
