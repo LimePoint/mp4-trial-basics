d5cd1d40f678c1ec90fd9732cd4c95fdaa8b6a58
PAVAN KALYAN BOYA <L159773@westpacgroup.com>
2025-11-20 13:39:34 +1100

    updated the actions

--- actions.rb
+++ actions.rb
@@ -150,6 +150,11 @@ end
 action :kv_properties do                                     
   log.info "First employee: #{OpsChain.properties.organization.employees[0].name}" 
 end
+
+action :file_properties do
+  log.info "Contents of file at /tmp/file1.txt - #{File.read('/tmp/file1.txt')}"
+end
+ 
  
  
  
