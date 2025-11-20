c1e401f5edf2d33a43a239003291839686474cd4
Neha Vashisht <neha.vashisht1@westpac.com.au>
2025-11-20 11:56:04 +1100

    Added the properties

--- actions.rb
+++ actions.rb
@@ -65,6 +65,11 @@ action :child_1 do
   action :file_properties do
     exec_command "cat /tmp/file1.txt"
   end
+
+  action :env_properties do
+    log.info "Value of MY_VARIABLE: #{ENV['MY_VARIABLE']}"
+    log.info "Value of ANOTHER_VARIABLE: #{ENV['ANOTHER_VARIABLE']}"
+  end 
   
   action :cleanup_temp_files do                 
 	if OpsChain.context.parents.environment&.code == 'dev1'                                                                                                                     
