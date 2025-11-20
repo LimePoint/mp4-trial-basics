adfe879f30f7c6fea85e41630fa157d7510b08e4
Naveen Yedla <naveen.yedla@westpac.com.au>
2025-11-20 11:55:32 +1100

    Updated latest file

--- actions.rb
+++ actions.rb
@@ -159,7 +159,6 @@ action :print_variable do
     log.info "Total number of employees: #{OpsChain.properties.to_yaml}"
   end
    
-  YAML
   action :kv_properties do                                     
     log.info "First employee: #{OpsChain.properties.organization.employees[0].name}" 
   end
@@ -167,4 +166,9 @@ action :print_variable do
   action :file_properties do
     exec_command "cat /tmp/file1.txt"
   end
+
+  action :env_properties do
+    log.info "Value of MY_VARIABLE: #{ENV['MY_VARIABLE']}"
+    log.info "Value of ANOTHER_VARIABLE: #{ENV['ANOTHER_VARIABLE']}"
+  end 
    
