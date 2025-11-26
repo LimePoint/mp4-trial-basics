7f6d1af3c828545284dc2fca5e9484fd602f8afb
Kishore Chatakonda <M058555@westpacgroup.com>
2025-11-26 13:05:21 +1100

    MP training - [OAHPINFRA-7477]

--- tomcat/actions.rb
+++ tomcat/actions.rb
@@ -1,5 +1,5 @@
 action :print_memory, description: 'prints memory from a file' do
 log.info "Memory:
-      #{OpsChain.properties.tomcat.minimum_memory}"
+      #{OpsChain.properties.tomcat.memory}"
 end
 
