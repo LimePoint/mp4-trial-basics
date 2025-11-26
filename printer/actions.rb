04c2f87a0ad5a40562860e62f68b9789945aaad6
Min Gu <M052909@westpacgroup.com>
2025-11-26 13:58:08 +1100

    add exercise for tomcat memory

--- printer/actions.rb
+++ printer/actions.rb
@@ -7,7 +7,8 @@ action :print_names_without_gui do
   log.info 'I can be executed as a step of another action or just directly via run_change'
 end
 
-action :print_all_names, description: 'prints all names', steps: [:print_names, :print_names_without_gui] do                                                          
+action :print_all_names, description: 'prints all names', steps: [:print_names, :print_names_without_gui] do
+  exec_command 'touch files/list_of_names.txt'                                                          
   exec_command 'cat files/list_of_names.txt'
 end
 
