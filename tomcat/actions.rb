2d82d7a3a2f0973f691cee36d72b9c1fe637b2a3
Sainath Chenepalle <sainath.chenepalle@westpac.com.au>
2025-11-26 12:59:08 +1100

    OAHPINFRA-7095-add-comments

--- tomcat/actions.rb
+++ tomcat/actions.rb
@@ -2,7 +2,6 @@
 #minimum_memory = 128 # Set this to the desired minimum memory value
 
 action :print_memory, description: 'prints memory usage from a file' do
-  #OpsChain.repository_properties_for(:environment)
   minimum_memory = OpsChain.properties.tomcat.minimum_memory                                                         
   puts "--------- Value of variable minimum_memory: #{minimum_memory}"
 end
