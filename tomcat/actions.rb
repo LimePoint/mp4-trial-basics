118c73cfce940530b774ae45eea408f3b34e12f1
CORPAU\M054575 <m054575@westpacgroup.com>
2025-11-26 12:46:19 +1100

    mem for env based props test val

--- tomcat/actions.rb
+++ tomcat/actions.rb
@@ -5,5 +5,5 @@ action :print_memory, description: 'prints memory usage from a file' do
   val = OpsChain.repository_properties_for(:environment)
   log.info "--------- Repository properties for environment: #{val.to_yaml}"
   minimum_memory = OpsChain.properties.tomcat.minimum_memory                                                         
-  puts "--------- Value of variable minimum_memory: #{minimum_memory}"
+  log.info "--------- Value of variable minimum_memory: #{minimum_memory}"
 end
