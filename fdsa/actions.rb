a1668fabe0c13e870e5440ab90c8e3e09fd57d7d
Min Gu <M052909@westpacgroup.com>
2025-11-26 11:44:27 +1100

    add exercise for tomcat memory

--- fdsa/actions.rb
+++ fdsa/actions.rb
@@ -1,4 +1,4 @@
 action :print_memory, description: 'prints memory configuratoin for the assigned env from fdsa' do                                                          
     memory_config =  OpsChain.properties.tomcat.memory
-    log.info "memory configuration for env #{OpsChain.context.parents.environment&.code}  is #{memory_config}GB"
+    log.info "memory configuration for env #{OpsChain.context.parents.environment&.code}  is #{memory_config}GB from fdsa"
 end
