47628cb84e6cd63eab2cbe9b225905055644d1b1
Santosh Kumar Pillella <L192379@westpacgroup.com>
2025-11-26 12:20:09 +1100

    addmem

--- tomcat/actions.rb
+++ tomcat/actions.rb
@@ -1,3 +1,3 @@
-action :print_memory, description: 'prints print_memory' do
+action :print_memory, description: 'print_memory' do
 log.info "Printing memory info: #{OpsChain.properties.tomcat.minimum_memory}"
 end
