ccef4e3cc7ab4f0e634229176a4a56ba503ee238
Naveen Yedla <naveen.yedla@westpac.com.au>
2025-11-26 11:17:27 +1100

    Updated file

--- tomcat/actions.rb
+++ tomcat/actions.rb
@@ -1,7 +1,3 @@
 action :print_memory do
-    log.info "Heap size for current environment: #{OpsChain.properties.memory.minimum_memory}"
-  end
-
-  action :print_memory1 do
     log.info "Heap size for current environment: #{OpsChain.properties.tomcat.minimum_memory}"
   end
