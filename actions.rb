66e82810fd5bf27973495808fe6ca73b042a983d
Aravind Kuttuva Krishna Chand <m066741@westpacgroup.com>
2026-06-04 14:06:05 +1000

    variables  - from Aravind

--- actions.rb
+++ actions.rb
@@ -59,3 +59,16 @@ end
 action :delete_user do
   exec_command 'userdel oracle'
 end
+
+my_name = 'foo'
+action :print_variable_1 do
+ puts "--------- Value of variable my_name: #{my_name}"
+ puts "--------- Value of variable my_address: #{my_address}"
+end
+
+my_address = 'bar'
+
+action :print_variable_2 do
+ puts "--------- Value of variable my_name: #{my_name}"
+ puts "--------- Value of variable my_address: #{my_address}"
+end
