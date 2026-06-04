4bbaf47e1f730b96a1f8ad7698ce21129f955800
Naren Puppala <naren.puppala@westpac.com.au>
2026-06-04 14:44:41 +1000

    update the script

--- actions.rb
+++ actions.rb
@@ -16,3 +16,26 @@ end
 action :find_errors do
   puts "I am missing an s in the puts and will error out"
 end
+
+action :use_logging do
+  puts "Current log level - #{log.level}"
+  puts "I am being printed via puts"
+  log.info "I am being printed via the logger as info"
+  log.warn "I am being printed via the logger as warn"
+  log.error "I am being printed via the logger as error"
+  log.fatal "I am being printed via the logger as fatal"
+  log.debug "I am being printed via the logger as debug"
+end
+
+
+
+action :get_cpu do
+  cpu = exec_command 'scripts/get_cpu.sh'
+
+  if cpu.success?
+    exec_command "scripts/print_cpu.sh"
+  else
+    puts "Failed to get CPU count"
+  end
+end
+
