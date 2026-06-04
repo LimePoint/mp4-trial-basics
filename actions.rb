f60400dfbf5d24d87c412276e6b7bbdf748a67c8
Apurva Nikam <L170287@westpacgroup.com>
2026-06-04 13:38:09 +1000

    Updated the actions.rb

--- actions.rb
+++ actions.rb
@@ -25,3 +25,8 @@ action :run_shell_commands_5 do
  exec_command '/tmp/non_existent_script.sh'
  puts "I will never get run"
 end
+
+action :run_shell_commands_5_1 ,ignore_failure: true do
+ exec_command '/tmp/non_existent_script.sh'
+ puts "I will never get run"
+end
