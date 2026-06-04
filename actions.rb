dcaa4deb1d31e8e1d8b79d9d40252a3af3a09849
Apurva Nikam <L170287@westpacgroup.com>
2026-06-04 14:23:01 +1000

    Updated the actions.rb

--- actions.rb
+++ actions.rb
@@ -74,10 +74,6 @@ action :print_variable_3 do
   puts "--------- Value of variable my_address: #{my_address}"
 end
 
-action :print_variable do
-  my_address = 'reset'
-  puts "--------- This is duplicate action"
-end
 
 action :find_errors do
   put "I am missing an s in the puts and will error out"
