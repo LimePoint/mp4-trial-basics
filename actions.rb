0bfa48ecea4f0b3b7bd8efe0c78b7c7793dc85d1
Aravind Kuttuva Krishna Chand <m066741@westpacgroup.com>
2026-06-04 15:19:59 +1000

    complex actions - from Aravind

--- actions.rb
+++ actions.rb
@@ -124,3 +124,12 @@ action :grandparent_2, steps: [:parent_1, :parent_2], run_as: :parallel  do
  log.info "I am grandparent 1"
 end
 
+action :grandchild_1 do
+  log.info "I am grand child action 1, I can be called independently.
+  But when called in prereq mode, I'll run before the calling action"
+end
+
+action parent_3: [:grandchild_1], steps: [:child_1, :child_2] do
+  log.info "I am parent action 1 and will now call child_1 and child_2 one by one."
+end
+
