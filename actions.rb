b2e7bb3b1f6968f91fd6264c7895ea61d51048bd
Apurva Nikam <L170287@westpacgroup.com>
2026-06-04 15:22:18 +1000

    Updated the actions.rb

--- actions.rb
+++ actions.rb
@@ -125,7 +125,7 @@ action :grandchild_2 do
   But when called in prereq mode, I'll run before the calling action"
 end
 
-action parent_3: [:grandchild_1], steps: [:child_1, :child_2] do
+action :parent_3 [:grandchild_1], steps: [:child_1, :child_2] do
   log.info "I am parent action 1 and will now call child_1 and child_2 one by one."
 end
  
