6e9b488e5545e5d524238b08c2ec3705bc952c9e
Naren Puppala <naren.puppala@westpac.com.au>
2026-06-04 13:30:53 +1000

    update the actions rb file

--- actions.rb
+++ actions.rb
@@ -16,7 +16,7 @@ action :run_system_health_commands do
 
   # System info
   exec_command 'echo "===== SYSTEM INFO ====="'
-  exec_command 'bin/hostname'
+  exec_command '/bin/hostname'
   exec_command 'uname -a'
   exec_command 'uptime'
 
@@ -63,3 +63,12 @@ action :run_shell_commands_3 do
  exec_command 'whoami' # this will not run as the previous command will fail
 end
 
+action :run_shell_commands_4 do
+ exec_command '/bin/false', abort_on_failure: false
+ exec_command 'whoami' # this will run as the previous command result do not matter
+end
+
+action :run_shell_commands_5 do
+ exec_command '/tmp/non_existent_script.sh'
+ puts “I will never get run”
+end
