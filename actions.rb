163c99a92230d03519961a7de9155521ad4c9aea
Apurva Nikam <L170287@westpacgroup.com>
2026-06-04 14:54:01 +1000

    Updated the actions.rb

--- actions.rb
+++ actions.rb
@@ -91,9 +91,9 @@ action :use_logging do
 end
 
 action :print_cpus do                                      
-  output = exec_command "bash scripts/get_cpu.sh", live_stream: false
+  output = exec_command "bash get_cpu.sh", live_stream: false
   cpu = output.stdout
   log.info "output from get_cpu - #{cpu}"
-  result = exec_command "bash scripts/print_cpu.sh #{cpu}", live_stream: false
+  result = exec_command "bash print_cpu.sh #{cpu}", live_stream: false
   log.info result.stdout
 end 
