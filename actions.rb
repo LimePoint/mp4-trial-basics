2c3eb291d13e59c41f25ad77df086f1aefbfc12c
Aravind Kuttuva Krishna Chand <m066741@westpacgroup.com>
2026-06-04 13:10:07 +1000

    exec command actions - from Aravind

--- actions.rb
+++ actions.rb
@@ -5,3 +5,13 @@ end
 action :run_shell_commands do
  exec_command 'whoami'
 end
+
+action :run_shell_commands_2 do
+ exec_command 'whoami'
+ exec_command 'meminfo'
+end
+
+action :run_shell_commands_3 do
+ exec_command '/bin/false'
+ exec_command 'whoami' # this will not run as the previous command will fail
+end
