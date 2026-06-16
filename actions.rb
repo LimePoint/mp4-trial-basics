a06d45243176fb009130d56b27b8cc34e9161abb
Apurva Nikam <L170287@westpacgroup.com>
2026-06-16 15:19:56 +1000

    Updated the actions.rb

--- actions.rb
+++ actions.rb
@@ -203,3 +203,17 @@ action :env_properties do
   log.info "Value of ANOTHER_VARIABLE: #{ENV['ANOTHER_VARIABLE']}"
 end 
  
+
+action :print_names, description: 'prints names from a file' do                                                          
+  exec_command 'cat files/list_of_names.txt'
+end
+ 
+action :print_names_without_gui do
+  log.info 'I will not been seen in the ui but I can be executed'
+  log.info 'I can be executed as a step of another action or just directly via run_change'
+end
+
+action :print_all_names, description: 'prints all names', steps: [:print_names, :print_names_without_gui] do                                                          
+  exec_command 'cat files/list_of_names.txt'
+end
+ 
