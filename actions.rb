cb5a1767cd4578a339e9b685ac4cfd1664a87129
Aravind Kuttuva Krishna Chand <m066741@westpacgroup.com>
2026-06-16 12:51:36 +1000

    adding env properties

--- actions.rb
+++ actions.rb
@@ -187,3 +187,9 @@ end
 action :file_properties do
   log.info "Contents of file at /tmp/file1.txt - #{File.read('/tmp/file1.txt')}"
 end
+
+action :env_properties do
+  log.info "Value of MY_VARIABLE: #{ENV['MY_VARIABLE']}"
+  log.info "Value of ANOTHER_VARIABLE: #{ENV['ANOTHER_VARIABLE']}"
+end 
+
