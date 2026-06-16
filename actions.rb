d44e7900f7017febcb1e78bcd9e8c7f95e944e40
Naren Puppala <naren.puppala@westpac.com.au>
2026-06-16 11:55:59 +1000

    update

--- actions.rb
+++ actions.rb
@@ -107,3 +107,7 @@ action :pod_step_write, steps: [:pod_step_read] do
   File.write('/tmp/file1.txt', "something")
   exec_command "ls -lrth /tmp"
 end
+
+action :print_context do
+  puts OpsChain.context.to_yaml
+end
